5a648e42b84d269d580c458f6325e22cae41a644
Enzo Vezzaro <enzo-vezzaro@live.it>
2020-07-10 21:09:18 +0200

    added routes and rename project

---
+++ ci-decomission.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Demolishing your awesome stacks..."
+
+cd candidatos
+serverless remove
+sleep 5s
+
+cd ..
+cd colegios
+serverless remove
+sleep 5s
+
+cd ..
+cd exterior
+serverless remove
+sleep 5s
+
+cd ..
+cd partidos
+serverless remove
+sleep 5s
+
+cd ..
+cd gateway
+serverless remove
+
+echo "Demolishing complete :)"
+read
