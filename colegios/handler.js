7ecf72ebf8bfaaab91f2ade973d3cafad2260272
Enzo Vezzaro <enzo-vezzaro@live.it>
2020-07-14 07:44:48 +0200

    added colegios

--- colegios/handler.js
+++ colegios/handler.js
@@ -1,27 +1,126 @@
 "use strict";
 
-module.exports.getColegios = async event => {
+// Require and initialize outside of your main handler
+const mysql = require('serverless-mysql')({
+  config: {
+    host     : process.env.ENDPOINT,
+    port     : process.env.PORT,
+    database : process.env.DATABASE,
+    user     : process.env.USERNAME,
+    password : process.env.PASSWORD
+  }
+});
+
+global.exit = (code, body) =>{
+  
+  if (!code || !body) {
+    code = 500,
+    body = [{
+      statusMessage: 'internal-error'
+    }];
+  } if (code == 400) {
+    code = 400,
+    body = [{
+      statusMessage: 'bad-request'
+    }];
+  } else {
+    if (body.length === 0) {
+      code = 404,
+      body = [{
+        statusMessage: 'not-found'
+      }];
+    } 
+  }
+
+  body.push({
+    _length: body.length
+  });
+
+  // Run close connection
+  mysql.quit();
+  
   return {
-    statusCode: 200,
-    body: JSON.stringify([
-      {
-        id: "72cd348d-3a9a-4173-a424-34908c43580a",
-        productName: "Foo 1",
-        price: 50,
-        customerName: "Allan"
-      },
-      {
-        id: "94df26f3-7acc-4b3f-a698-a28707f90f04",
-        productName: "Foo 2",
-        price: 40,
-        customerName: "Laura"
-      },
-      {
-        id: "5931c2f2-7345-4820-93ce-0cb5907a361b",
-        name: "Foo 3",
-        price: 21,
-        customerName: "Tom"
-      }
-    ])
+    statusCode: code,
+    body: JSON.stringify(body)
   };
+};
+
+module.exports.getColegios = async event => {
+  console.log('Event: ', event);
+
+  let init = {
+    start: 0,
+    limit: 50
+  }
+
+  // Run your query
+  let query = `
+    SELECT * FROM opendatadb.FUSIONES_COLEGIOS_07_2020
+  `;
+
+  // Get Path Params
+  let pathParameters = event['pathParameters'];
+  // Log Params
+  console.log('Path Params: ', pathParameters);
+  if (pathParameters){
+    // Check if there's year and it's valid
+    if (pathParameters.year){
+      let year = parseInt(pathParameters.year);
+      // if the year is not available, return message
+      if (
+        year !== 2020
+      ){
+        return global.exit(404, []);
+      }
+    }
+  }
+
+  let queryStringParameters = event['queryStringParameters'];
+  // Log Params
+  console.log('Query Params: ', queryStringParameters);
+  if (queryStringParameters){
+    // If there's query available, filter by query value
+    if (queryStringParameters.query){
+      // Return if there's no value
+      if (!queryStringParameters.value) global.exit(400, []);
+      // Make sure it's a string
+      String(queryStringParameters.value);
+      // Make query
+      query += ` WHERE ${queryStringParameters.query} like '%${queryStringParameters.value}%'`;
+    }
+
+    // If the start param has been passed (pagination)
+    if (queryStringParameters.start){
+      // Return if there's no value
+      if (isNaN(queryStringParameters.start)) global.exit(400, []);
+      // Return 50 records from the new start
+      init.start = queryStringParameters.start;
+    }
+
+    // add del
+    if (queryStringParameters.query){
+      query += ` AND _del = 0`;
+    } else {
+      query += ` WHERE _del = 0`;
+    }
+    
+  }  
+
+  // add limit
+  query += ` LIMIT ${init.start},${init.limit};`;
+
+  // Log query
+  console.log('Query: ', query);
+  
+  // Make query
+  let results;
+  try {
+    results = await mysql.query(query); 
+    // Exit from lambda
+    return global.exit(200, results);  
+  } catch (error) {
+    // Exit from lambda
+    return global.exit(400, []);  
+  }
+  
 };
