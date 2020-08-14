aefed5aff0f796ee9d8d1d8c8cbd6bfae4bea553
Enzo Vezzaro <enzo-vezzaro@live.it>
2020-08-14 16:33:08 +0200

    added map endpoints

--- jce/elecciones/handler.js
+++ jce/elecciones/handler.js
@@ -1,5 +1,15 @@
 "use strict";
 
+const { Client } = require('@elastic/elasticsearch');
+
+let token = Buffer.from(`${process.env.ELASTICSEARCH_USER}:${process.env.ELASTICSEARCH_PWD}`, 'utf8').toString('base64');
+const client = new Client({ node: `${process.env.ELASTICSEARCH_ENDPOINT}`, 
+  headers: { 
+    'Content-Type': 'application/json',
+    'Authorization': `Basic ${token}`
+  } 
+});
+
 // Require and initialize outside of your main handler
 const mysql = require("serverless-mysql")({
   config: {
@@ -267,155 +277,128 @@ module.exports.getEleccionesMunicipales = async (event) => {
 };
 
 module.exports.getEleccionesPresidenciales = async (event) => {
-  return global.exit(200, [{ statusMessage: "not yet available, stay tune!" }]);
-  console.log("Event: ", event);
+  
+  /** Immediate response for WarmUP plugin */
+  if (event.source === 'serverless-plugin-warmup') {
+    console.log('WarmUP - Lambda is warm!')
+    return global.exit(800, []);
+  }
 
   let init = {
-    start: 0,
-    limit: 50,
-  };
-
-  // Run your query
-  let query = `
-    SELECT distinct(pais_votacion), _source FROM opendatadb.EXTERIOR_14_07_2020
-  `;
+    index: 'jce-ganadores-elecciones-jul-2020',
+    from: 0,
+    size: 50
+  }
 
   // Get Path Params
-  let pathParameters = event["pathParameters"];
-  // Log Params
-  console.log("Path Params: ", pathParameters);
-  if (pathParameters) {
-    // Check if there's year and it's valid
-    if (pathParameters.year) {
-      let year = parseInt(pathParameters.year);
-      // if the year is not available, return message
-      if (year !== 2020) {
-        return global.exit(404, []);
-      }
-    }
-  }
-
-  let queryStringParameters = event["queryStringParameters"];
-  // Log Params
-  console.log("Query Params: ", queryStringParameters);
-  if (queryStringParameters) {
-    // If there's query available, filter by query value
-    if (queryStringParameters.query) {
-      // Return if there's no value
-      if (!queryStringParameters.value) global.exit(400, []);
-      // Make sure it's a string
-      String(queryStringParameters.value);
-      // Make query
-      query += ` WHERE ${queryStringParameters.query} like '%${queryStringParameters.value}%'`;
-    }
-
+  let pathParameters = event['queryStringParameters'];
+  // Log query
+  console.log('Path Params: ', pathParameters);
+  if (pathParameters){
     // If the start param has been passed (pagination)
-    if (queryStringParameters.start) {
+    if (pathParameters.start){
       // Return if there's no value
-      if (isNaN(queryStringParameters.start)) global.exit(400, []);
+      if (isNaN(pathParameters.start)) global.exit(400, []);
       // Return 50 records from the new start
-      init.start = queryStringParameters.start;
+      init.from = pathParameters.start;
     }
-
-    // add del
-    if (queryStringParameters.query) {
-      query += ` AND _del = 0`;
-    } else {
-      query += ` WHERE _del = 0`;
-    }
-  }
-
-  // add limit
-  query += ` LIMIT ${init.start},${init.limit};`;
+  } 
 
   // Log query
-  console.log("Query: ", query);
+  /*
+  let query = {
+    "match_all": {}
+  };
+  */
 
-  // Make query
-  let results;
+  let query = {
+    match: {
+      PROVINCIA: 'NACIONAL'
+    }
+  };
+
+  console.log('query: ', query);
+
   try {
-    results = await mysql.query(query);
+    // callback API
+    let result = await client.search({
+      index: init.index,
+      from: init.from,
+      size: init.size,
+      body: {
+        query
+      }
+    })
+
+    let hits = result['body']['hits']['hits'];  
     // Exit from lambda
-    return global.exit(200, results);
+    return global.exit(200, hits);  
   } catch (error) {
     // Exit from lambda
-    return global.exit(400, []);
+    return global.exit(400, []);  
   }
 };
 
 module.exports.getEleccionesCongresuales = async (event) => {
-  return global.exit(200, [{ statusMessage: "not yet available, stay tune!" }]);
-  console.log("Event: ", event);
+  
+  /** Immediate response for WarmUP plugin */
+  if (event.source === 'serverless-plugin-warmup') {
+    console.log('WarmUP - Lambda is warm!')
+    return global.exit(800, []);
+  }
 
   let init = {
-    start: 0,
-    limit: 50,
-  };
-
-  // Run your query
-  let query = `
-    SELECT distinct(demarcacion), _source FROM opendatadb.EXTERIOR_14_07_2020
-  `;
+    index: 'jce-ganadores-elecciones-jul-2020',
+    from: 0,
+    size: 50
+  }
 
   // Get Path Params
-  let pathParameters = event["pathParameters"];
-  // Log Params
-  console.log("Path Params: ", pathParameters);
-  if (pathParameters) {
-    // Check if there's year and it's valid
-    if (pathParameters.year) {
-      let year = parseInt(pathParameters.year);
-      // if the year is not available, return message
-      if (year !== 2020) {
-        return global.exit(404, []);
-      }
-    }
-  }
-
-  let queryStringParameters = event["queryStringParameters"];
-  // Log Params
-  console.log("Query Params: ", queryStringParameters);
-  if (queryStringParameters) {
-    // If there's query available, filter by query value
-    if (queryStringParameters.query) {
-      // Return if there's no value
-      if (!queryStringParameters.value) global.exit(400, []);
-      // Make sure it's a string
-      String(queryStringParameters.value);
-      // Make query
-      query += ` WHERE ${queryStringParameters.query} like '%${queryStringParameters.value}%'`;
-    }
-
+  let pathParameters = event['queryStringParameters'];
+  // Log query
+  console.log('Path Params: ', pathParameters);
+  if (pathParameters){
     // If the start param has been passed (pagination)
-    if (queryStringParameters.start) {
+    if (pathParameters.start){
       // Return if there's no value
-      if (isNaN(queryStringParameters.start)) global.exit(400, []);
+      if (isNaN(pathParameters.start)) global.exit(400, []);
       // Return 50 records from the new start
-      init.start = queryStringParameters.start;
+      init.from = pathParameters.start;
     }
-
-    // add del
-    if (queryStringParameters.query) {
-      query += ` AND _del = 0`;
-    } else {
-      query += ` WHERE _del = 0`;
-    }
-  }
-
-  // add limit
-  query += ` LIMIT ${init.start},${init.limit};`;
+  } 
 
   // Log query
-  console.log("Query: ", query);
+  let query = {
+    "match_all": {}
+  };
 
-  // Make query
-  let results;
+  if (pathParameters){
+    let value = pathParameters.value;
+    query = {
+      match: {
+        [pathParameters.query] : value.toUpperCase() 
+      }
+    };
+  }
+
+  console.log('query: ', query);
+
   try {
-    results = await mysql.query(query);
+    // callback API
+    let result = await client.search({
+      index: init.index,
+      from: init.from,
+      size: init.size,
+      body: {
+        query
+      }
+    })
+
+    let hits = result['body']['hits']['hits'];  
     // Exit from lambda
-    return global.exit(200, results);
+    return global.exit(200, hits);  
   } catch (error) {
     // Exit from lambda
-    return global.exit(400, []);
+    return global.exit(400, []);  
   }
 };
