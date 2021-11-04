f7839211e0a777f23a395a560fca507168d72d9a
Enzo Vezzaro <enzo-vezzaro@live.it>
2021-11-04 19:16:53 +0100

    improvements services and departments

--- src/departments/index.js
+++ src/departments/index.js
@@ -1,10 +1,42 @@
 const axios = require('axios');
-const utils = require('../utils');
+const utils = require('../services');
 
-const minerd = require('./minerd/center');
+const admin = require('./minerd/admin');
+const budget = require('./minerd/budget');
+const hr = require('./minerd/hr');
 
-exports.dep_minerd = async function(req, res) {
-  const allResults = await minerd.findByAll();
+const exit = ((res, code, body)=>{
+  return res.send({
+    code,
+    body
+  })
+})
+
+exports.getDepartments = async (req, res) => {
+  console.log('starting getDepartments ------');
+  const { params, query } = req; 
+  console.log('getDepartments - get params ------', params, query);
+
+  /*
+  http://localhost:8000
+  /v2/services/search/ipfs/
+  bafybeifds2zwfw7zn7gbh7oa2z23xyuyg6rbs6xizbsvxtg5xrfl4quo3u?
+  fields=TOTAL&
+  value=4&
+  limit=44&
+  info=true
+  */
+
   
-  res.send(allResults);
-};
+
+  await admin.findByAll()
+  .then((results)=>{
+    console.log('done csvtojson -------');
+    exit(res, 200, results);
+  })
+  .catch(err => {
+    console.log('error csvtojson -------');
+    exit(res, 500, err);
+  })
+  console.log('done csvtojson ------');
+}
