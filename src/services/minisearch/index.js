b2c5276ea8776dc758c7aef670177fd9bff92281
Enzo Vezzaro <enzo-vezzaro@live.it>
2022-04-18 17:10:39 -0400

    added screenshot endpoint + refactor search + clean up

--- src/services/minisearch/index.js
+++ src/services/minisearch/index.js
@@ -1,57 +1,113 @@
-const transformCSV = require('../transformCSV');
 const MiniSearch = require('minisearch');
 
 const utils = require('../../utils');
+const Papa = require('papaparse');
+const request = require("request");
 
 const queryFile = async (params, query) => {
   return new Promise(async (resolve, reject) => {
     //console.log('getting params and query: ', params, query);
     if (!params.cid) { reject('missing cid'); return;}
-    if (!params.type) { reject('missing file type'); return;}
     if (!params.host) { reject('missing host'); return;}
-    // file type
-    const fileType = ['csv'];
-    if (!fileType.includes(params.type)) { reject('wrong type'); return; }
-    // http://localhost:8000/services/transform-csv/ipfs/bafybeifds2zwfw7zn7gbh7oa2z23xyuyg6rbs6xizbsvxtg5xrfl4quo3u
-    // set host on params as it's a IPFS file
-    // console.log('queryFile - get params -----', params);
-    try {
-      const getJsonFromCID = await transformCSV.CSVtoJSON(params);
-      let data = getJsonFromCID.body;
-      // console.log('data: ', data);
-      if (data){
+
+    const cors_anywhere = 'https://cors-aletheiadata.herokuapp.com';
+    
+      let url;
+      if (params.host === 'ipfs'){
+        // if it's IFPS file
+        const file = params.cid + '.ipfs.dweb.link';  
+        url = `https://${file}`
+      } else if (params.host === 'http'){
+        // WARNING: experimental feature
+        // this queries on a remote file
+        url = `${cors_anywhere}/${params.cid}`
+      } else {
+        console.log('type: ', params.host);
+        return {
+          code: 400,
+          body: 'wrong type'
+        }
+      }
+
+      console.log('transforming file from url: ', url);
+      const options = {
+        header: true,
+        encoding: "latin1"
+      };
+
+      let contentType;
+      let contentLenght;
+      const parseStream = Papa.parse(Papa.NODE_STREAM_INPUT, options);
+      const dataStream = request
+        .get(url)
+        .on('response', function(response) {
+          contentType = response.headers['content-type'];
+          contentLenght = response.headers['content-length'];
+        })
+        .pipe(parseStream);
+
+      let data = [];
+      let counter = 0;
+      let fields;
+      let miniSearch;
+      let isTrueSet = query.info === 'true';
+      parseStream.on("data", async chunk => {
+        data.push(chunk);
         // add ids
-        /* TODO: understand what to do with r.Nombre */
-        data.map((r, i) => { r.id = i; if (r.Nombre) return r; });
         // get fields
-        const fields = Object.keys(getJsonFromCID.body[0]);
-        console.log('queryFile - get header fields: ', fields);
+        fields = Object.keys(chunk);
+        // console.log('queryFile - get header fields: ', fields);
         // if info is tru, return all the info of the doc
-        var isTrueSet = (query.info === 'true');
         if (isTrueSet){
-          resolve({
+          return;
+        }
+
+        // queryFile
+        if (!miniSearch){
+          console.log('creating minisearch ------ ');
+          miniSearch = new MiniSearch({
+            fields: fields, // fields to index for full-text search
+            storeFields: fields, // fields to return with search results
+            searchOptions: {
+              fuzzy: query.fuzzy ? query.fuzzy : 3
+            } 
+          })
+        }
+
+        // adding id to record
+        chunk.id = counter;
+        counter ++;
+
+        // adding to minisearch
+        await miniSearch.addAll([chunk]);
+        // console.log('minisearch: ', miniSearch);
+      });
+
+      dataStream.on("finish", async () => {
+
+        if (isTrueSet){
+          // if set, return file's info
+          return resolve({
             cid: params.cid,
-            fileType: params.type,
+            type: contentType,
+            length: contentLenght,
             host: params.host,
             fields: fields
           });
-          return;
         }
-        // queryFile
-        miniSearch = new MiniSearch({
-          fields: fields, // fields to index for full-text search
-          storeFields: fields, // fields to return with search results
-          searchOptions: {
-            fuzzy: 3
-          } 
-        })
-        // Index all documents
-        await miniSearch.addAll(data);
+        
+        // if data in empty = no file
+        if (data.length == 0){
+          return reject({
+            code: 400,
+            body: 'file not found'
+          })
+        }
         // Search with default options
         // Search only specific fields
-        // init searchm, sorting, filtering
+        // init search, sorting, filtering
         let queryResult;
-        console.log('query: ', query);
+        console.log(`queryFile - query [${JSON.stringify(query)}]`);
         if (!query.fields && !query.value){
           // get all
           resolve(data);
@@ -65,53 +121,88 @@ const queryFile = async (params, query) => {
             resolve('missing value');
             return;
           }
-          console.log('queryFile - query on fields: ', query.fields);
-          console.log('queryFile - query value: ', query.value);
+          console.log(`queryFile - query on fields [${query.fields}]`);
+          console.log(`queryFile - query value [${query.value}]`);
           // get array from query string
           const query_fields = query.fields.split(',');
           query_fields.map((qf)=>{
             // check if the field pass exists on the file
-            console.log(qf);
             if (!fields.includes(qf)){
               console.log(fields.includes(qf));
               resolve('one or more fields are not valid');
               return;
             }
           })
+          console.log(`queryFile - query fields ${JSON.stringify(query_fields)}`);
           // get number of results
-          if (query.limit){
-            console.log('queryFile - query limit: ', query.limit);
-            if (!utils.isNumber(query.limit)) {
-              if (query.limit !== 'none'){
-                resolve('limit not valid');
-                return;
+          queryResult = await miniSearch.search(query.value, { fields: query_fields });
+          // console.log('here -----> ', queryResult);
+          if (queryResult.length > 0){
+            // order by
+            if (query.order){
+              const orderParts = (query.order).split(':');
+              /**
+               * orderParts[0] = field
+               * orderParts[1] = order (asc/desc)
+               */
+              console.log(`queryFile - query order by user [${query.order}]`);
+              queryResult = await queryResult.sort(function(a, b) { 
+                return a[orderParts[0]] - b[orderParts[0]];
+              });
+              
+              if (orderParts[1]){
+                console.log(`queryFile - query order by [${orderParts[0]} : ${orderParts[1]}]`);
+                switch (orderParts[1]) {
+                  case 'asc':
+                    queryResult = queryResult;
+                    break;
+                  case 'desc':
+                    queryResult = queryResult.reverse();
+                    break;
+                  default:
+                    queryResult = queryResult;
+                    break;
+                }
               } else {
-                // check if limit = 'none', if so give all results
-                console.log('queryFile - CAUTION - query without limit');
-                queryResult = await miniSearch.search(query.value, { fields: query_fields });
+                console.log(`queryFile - query order by [${orderParts[0]} : ${orderParts[1]}]`);
+                queryResult = queryResult.reverse();
               }
             } else {
-              console.log('queryFile - query limit by user');
-              queryResult = await miniSearch.search(query.value, { fields: query_fields }).slice(0, query.limit);
+              console.log('queryFile - query order default [ID]');
+              queryResult = queryResult.sort(function(a, b) { 
+                return a['id'] - b['id'];
+              });
             }
-          } else {
-            // default: 25 results
-            console.log('queryFile - query default limit: 25 records');
-            queryResult = await miniSearch.search(query.value, { fields: query_fields }).slice(0, 25);
+            // limit by
+            query.start = query.start ? query.start : 0;
+            if ((query.limit !== '0' && query.limit !== 0) && query.limit){
+              console.log(`queryFile - query limit [${query.limit}]`);
+              console.log(`queryFile - query start [${query.start}]`);
+              if (!utils.isNumber(query.limit)) {
+                if (query.limit !== 'none'){
+                  resolve('limit not valid');
+                  return;
+                } else {
+                  // check if limit = 'none', if so give all results
+                  console.log('queryFile - CAUTION - query without limit');
+                  queryResult = queryResult.slice(0, query.limit);
+                }
+              } else {
+                console.log(`queryFile - query limit by user [start: ${query.start} - limit: ${query.limit}]`);
+                queryResult = queryResult.slice(query.start,parseInt(query.start)+parseInt(query.limit));
+              }
+            } else {
+              // default: 25 results
+              console.log('queryFile - query default limit [25 records]', query.start);
+              queryResult = queryResult.slice(query.start,parseInt(query.start)+25);
+            }
           }
+          
+          // return result
+          return resolve(queryResult);
+          
         }
-        // return result
-        resolve(queryResult);
-        return;
-      } else {
-        reject('no data from cid ------');
-        return;
-      }
-      // make query with 
-    } catch (error) {
-      reject(error)
-      return;
-    }
+      });
   });
 };
 
--- src/services/screenshot/index.js
+++ src/services/screenshot/index.js
@@ -1,12 +1,43 @@
-const fs = require('fs');
-const path = require('path');
-const utils = require('../../utils');
+// import { Web3Storage } from 'web3.storage'
+const web3Storage = require('web3.storage');
 
 const { v4: uuidv4 } = require('uuid');
 
 const Nightmare = require('nightmare');
 
 const compress_images = require("compress-images");
+
+function getAccessToken () {
+  // If you're just testing, you can paste in a token
+  // and uncomment the following line:
+  // return 'paste-your-token-here'
+
+  // In a real app, it's better to read an access token from an
+  // environement variable or other configuration that's kept outside of
+  // your code base. For this to work, you need to set the
+  // WEB3STORAGE_TOKEN environment variable before you run your code.
+  return process.env.WEB3_STORAGE_API_KEY
+}
+
+function makeStorageClient () {
+  return new web3Storage.Web3Storage({ token: getAccessToken() })
+}
+
+async function getFiles (path) {
+  const files = await web3Storage.getFilesFromPath(path)
+  console.log(`read ${files.length} file(s) from ${path}`)
+  return files
+}
+
+async function storeFiles (path) {
+  const files = await getFiles(path);
+  const client = makeStorageClient()
+  const cid = await client.put(files,{
+    wrapWithDirectory: false
+  })
+  console.log('stored files with cid:', cid)
+  return cid
+}
 
 const makeScreenshot = async (params, query) => {
   return new Promise(async (resolve, reject) => {
@@ -86,6 +117,88 @@ const makeScreenshot = async (params, query) => {
   });
 };
 
+const certScreenshot = async (params, query) => {
+  return new Promise(async (resolve, reject) => {
+    //console.log('getting params and query: ', params, query);
+    if (!params.width) { reject('missing width'); return;}
+    if (!params.height) { reject('missing height'); return;}
+
+    if (!query || !query.url) { reject('missing url'); return;}
+
+    if (params.width == 0 || isNaN(params.width)) {reject('wrong width'); return;}
+    if (params.height == 0 || isNaN(params.height)) {reject('wrong height'); return;}
+
+    const name = uuidv4();
+    const width = parseInt(params.width)
+    const height = parseInt(params.height)
+
+    var nightmare = new Nightmare({
+      show: false,
+      width: width,
+      height: height
+    });
+
+    var dimensions = await nightmare.goto(query.url)
+      .wait('body')
+      .evaluate(function() {
+          var body = document.querySelector('body');
+          var timestamp = `<p style="
+            position: absolute; 
+            bottom: 0; 
+            left: 0;
+            background-color: #fff;
+            padding: 30px 50px;
+            font-size: 14px;
+          ">
+            Certified by Aletheia Systems <br />
+            ${(new Date()).toString()}
+          </p>`;
+          body.innerHTML = body.innerHTML + timestamp;
+          return {
+              height: document.body.clientHeight,
+              width: document.body.clientWidth
+          }
+      });
+    
+    await nightmare.viewport(dimensions.width, dimensions.height)
+      .screenshot(`./screenshot/${name}.png`);
+    
+    await nightmare.end();
+
+    await compress_images(
+      `./screenshot/${name}.png`,
+      "./compress/",
+      { 
+        compress_force: false, 
+        statistic: true, 
+        autoupdate: true 
+      }, false,
+      { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+      { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+      { svg: { engine: "svgo", command: "--multipass" } },
+      { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] },
+      },async function (err, completed) {
+        if (completed === true) {
+          const cid = await storeFiles(`./screenshot/${name}.png`)
+          // resolve query
+          resolve({
+            cid: `${cid}`,
+            metadata: {
+              width: width,
+              height: height,
+              url: query.url
+            }
+          });
+        }
+      }
+    );
+    
+    // resolve(`${name}.${params.format}`);
+
+  });
+};
+
 module.exports = {
-  makeScreenshot
+  makeScreenshot,
+  certScreenshot
 }
